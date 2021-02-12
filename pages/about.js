e7d6a20a6a5fb57cc836ffa318ce37722664b160
tahin.daniel@gmail.com <tahin.daniel@gmail.com>
2021-02-12 16:59:37 +0100

    part one

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed similique quis minima tenetur itaque! Autem voluptatem et accusamus nulla neque ducimus voluptatum est architecto voluptates. Numquam itaque nulla perferendis esse?</p>
+        </div>
+     );
+}
+ 
+export default About;
