ed33b494d5da3ffb7de56644eee49d3d419341f1
tahin.daniel@gmail.com <tahin.daniel@gmail.com>
2021-02-16 13:23:55 +0100

    part two

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Home</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique velit obcaecati, non optio nemo iste modi, sed, ullam vero aliquid corporis aut perspiciatis temporibus suscipit accusamus laudantium eius ab fugiat!</p>
-      <Link href="/ninjas/test">
-        <a className={styles.btn}>All ninjas</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1 className={styles.title}>Home</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique velit obcaecati, non optio nemo iste modi, sed, ullam vero aliquid corporis aut perspiciatis temporibus suscipit accusamus laudantium eius ab fugiat!</p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>All ninjas</a>
+        </Link>
+      </div>
+    </>
   )
 }
